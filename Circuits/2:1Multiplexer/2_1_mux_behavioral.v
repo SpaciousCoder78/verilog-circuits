8d3f7e2ebadd3e99775e31c7d761dedcd3ed8589
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-07-27 18:51:08 +0530

    Create 2_1_mux_behavioral.v

---
+++ Circuits/2:1Multiplexer/2_1_mux_behavioral.v
@@ -0,0 +1,14 @@
+//2:1 Multiplexer Behavioral
+
+module mux_2_1_bh(Y,I,S);
+  input [1:0}I;
+  input S;
+  output reg Y;
+
+  always@(*)
+    if (S)
+       Y=I[1];
+    else
+        Y=I[0];
+endmodule
+         
